ce4381c63041bfad2e81c0ee095b58443cb2ba14
Mario Amaya <thefozcat@gmail.com>
2019-05-16 13:39:53 -0700

    added addUser to users-model, users-router

--- users/users-model.js
+++ users/users-model.js
@@ -2,10 +2,24 @@ const db = require('../data/dbConfig');
 
 module.exports = {
     getUsers,
-    // addUser,
-    // deleteUser
+    addUser,
+    deleteUser
 }
 
 function getUsers() {
     return db('users');
+}
+
+function getUserById(id) {
+    return db('users')
+        .where({ id })
+        .first()
+}
+
+function addUser(user) {
+    return db('users')
+        .insert(user, 'id')
+        .then(([id]) => {
+            return getUserById(id)
+        })
 }
--- users/users-router.js
+++ users/users-router.js
@@ -12,4 +12,18 @@ router.get('/', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    if (!req.body.name || !req.body.email) {
+        res.status(400).json({ errorMessage: 'A username and email are required.' })
+    } else {
+        Users.addUser(req.body)
+            .then(user => {
+                res.status(200).json(user)
+            })
+            .catch(err => {
+                res.status(500).json({ error: err, message: 'This user already exists in the database.'})
+            })
+    }
+})
+
 module.exports = router;
